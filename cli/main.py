5ddf5cc0315405396219a8ec702b388a6b4df442
Tom Jansen <tomjansen25@gmail.com>
2023-04-29 21:36:51 +0200

    poetry add typer, cli as separate folder

---
+++ cli/main.py
@@ -0,0 +1,25 @@
+import typer
+from beerspider.spiders.beers.bierlinie_manual import BierlinieManualSpider
+from beerspider.spiders.beers.flaschenpost_manual import FlaschenpostManualSpider
+from beerspider.spiders.beers.rewe_manual import ReweShopSpider
+
+app = typer.Typer(name="beer-scraper-cli", no_args_is_help=True)
+
+
+@app.command()
+def test_cli():
+    typer.echo("Hi!")
+
+
+@app.command()
+def run_bierlinie_spider(headless: bool = True):
+    bierlinie_spider = BierlinieManualSpider(scrape_headless=headless)
+    bierlinie_spider.parse_urls()
+    bierlinie_spider.export_results()
+
+
+@app.command()
+def run_flaschenpost_spider(headless: bool = True):
+    flaschenpost_spider = FlaschenpostManualSpider(scrape_headless=headless)
+    flaschenpost_spider.parse_urls()
+    flaschenpost_spider.export_results()
